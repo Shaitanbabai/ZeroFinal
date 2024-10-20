d2dd0daa515a5a618fdaf6cb706d2b22e2581cdd
Shaitanbabai <165654410+Shaitanbabai@users.noreply.github.com>
2024-10-20 16:35:06 +0300

    fixed views and urls in business_app , added 404 treatment

--- myproject/business_app/urls.py
+++ myproject/business_app/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from . import views
 
+# Укажите путь к вашей функции custom_404
+handler404 = 'business_app.views.custom_404'
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('authorization/', views.authorization, name='authorization'),
+    path('authorization/', views.AuthorizationView.as_view(), name='authorization'),
     path('purchase/', views.purchase, name='purchase'),
     path('sale/', views.sale, name='sale'),
     path('profile/', views.profile, name='profile'),
-    path('registration/', views.registration, name='registration'),
+    path('registration/', views.RegistrationView.as_view(), name='registration'),
     path('logout/', views.logout_view, name='logout'),
     # Добавьте другие URL-маршруты, необходимые для вашего приложения
 ]
+
--- myproject/business_app/views.py
+++ myproject/business_app/views.py
@@ -3,77 +3,100 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
 from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.urls import reverse
+import logging
 
+# Настройка логгера
+logger = logging.getLogger(__name__)
 
 def main_page(request):
     return render(request, 'business_app/main_page.html')
 
+class AuthorizationView(View):
+    def get(self, request):
+        return render(request, 'business_app/authorization.html')
 
-def authorization(request):
-    if request.method == 'POST':
+    def post(self, request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         user = authenticate(request, username=email, password=password)
 
         if user is not None:
             login(request, user)
+            logger.info(f"User {user.username} logged in.")
             return redirect_user_based_on_group(user)
         else:
-            # Обработка ошибки аутентификации (например, неверный логин или пароль)
             context = {'error': 'Invalid email or password'}
             return render(request, 'business_app/authorization.html', context)
-    return render(request, 'business_app/authorization.html')
-
 
 @login_required
 def purchase(request):
-    if request.user.groups.filter(name='customer').exists():
+    group_names = request.user.groups.values_list('name', flat=True)
+    if 'customer' in group_names:
         context = {'is_customer': True}
         return render(request, 'business_app/purchase.html', context)
-    elif request.user.groups.filter(name='salesman').exists():
-        return redirect('sale')
+    elif 'salesman' in group_names:
+        return redirect(reverse('sale'))
     else:
-        return redirect('main_page')
-
+        return redirect(reverse('main_page'))
 
 @login_required
 def sale(request):
-    if request.user.groups.filter(name='salesman').exists():
+    group_names = request.user.groups.values_list('name', flat=True)
+    if 'salesman' in group_names:
         context = {'is_salesman': True}
         return render(request, 'business_app/sale.html', context)
-    elif request.user.groups.filter(name='customer').exists():
-        return redirect('purchase')
+    elif 'customer' in group_names:
+        return redirect(reverse('purchase'))
     else:
-        return redirect('main_page')
-
+        return redirect(reverse('main_page'))
 
 @login_required
 def profile(request):
     return render(request, 'business_app/profile.html')
 
+class RegistrationView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'business_app/registration.html', {'form': form})
 
-def registration(request):
-    if request.method == 'POST':
+    def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user= form.save()
+            # Удален блок try-except
             customer_group = Group.objects.get(name='customer')
             user.groups.add(customer_group)
             login(request, user)
-            return redirect('purchase')
-    else:
-        form = UserCreationForm()
-    return render(request, 'business_app/registration.html', {'form': form})
+            logger.info(f"New user {user.username} registered and logged in.")
+            return redirect(reverse('purchase'))
+        else:
+            return render(request, 'business_app/registration.html', {'form': form, 'errors': form.errors})
 
 def logout_view(request):
+    logger.info(f"User {request.user.username} logged out.")
     logout(request)
-    return redirect('main_page')
+    return redirect(reverse('main_page'))
 
 def redirect_user_based_on_group(user):
     """Redirect user based on their group."""
     if user.groups.filter(name='customer').exists():
-        return redirect('purchase')
+        return redirect(reverse('purchase'))
     elif user.groups.filter(name='salesman').exists():
-        return redirect('sale')
+        return redirect(reverse('sale'))
     else:
-        return redirect('profile')
+        return redirect(reverse('profile'))
+
+# Обработчик 404 ошибки
+def custom_404(request, _exception):
+    # Проверка, авторизован ли пользователь
+    if request.user.is_authenticated:
+        # Проверка группы, к которой принадлежит пользователь
+        if request.user.groups.filter(name='customer').exists():
+            return render(request, 'business_app/purchase.html')
+        elif request.user.groups.filter(name='salesman').exists():
+            return render(request, 'business_app/sale.html')
+
+    # Для неавторизованных пользователей или если пользователь не в указанных группах
+    return render(request, 'business_app/page_404.html')
